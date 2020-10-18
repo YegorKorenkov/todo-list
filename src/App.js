ec8a9449e7a3c140c2d55e56809072f8e361b937
Yegor Korenkov <eg.korenkov00@gmail.com>
2020-10-18 15:14:57 +0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    todoValue: "",
+    todos: [],
+    
+  }
+
+
+
+  addTodo(todoValue) {
+    if (todoValue.trim()) {
+      this.setState({
+        todoValue: "",
+        todos: [...this.state.todos, { text: todoValue, check: false }]
+      })
+    } else {
+      alert("Write something");
+      this.setState({todoValue: ""})
+    }
+    
+  }
+
+  removeTodo(todoKey) {
+    this.setState({ todos: [...this.state.todos.filter((todo, key) => todo.check !== true)]})
+  }
+
+  todoChecked = (todo, key) => {
+    
+    const todos = [...this.state.todos]
+    const todoCheck = {...todos}
+    const xxx = todo.check
+    todoCheck[key].check = !xxx
+    todos[key] = todoCheck[key]
+    
+
+    // const todos = [...this.state.todos]
+    // const todoCheck = {...todos}   
+    
+    // let todoKey
+    // todos.forEach((item, index) => {
+    //     if (item.text === todo.text) {
+    //     todoKey = index
+    //   }
+    // })
+
+    // const xxx = todo.check
+    // todoCheck[todoKey].check = !xxx
+    // todos[todoKey] = todoCheck[todoKey]
+
+    //console.log(todoKey)
+    this.setState({
+      todos: todos
+    })
+  }
+
+
+  render () {
+    return (
+
+      <div>
+        <input type="text" value={this.state.todoValue} onChange={e => this.setState({todoValue: e.target.value})}></input>
+        <button onClick={ e => this.addTodo(this.state.todoValue)}>Click</button>
+        
+        <ul>
+          {this.state.todos.map((todo, key) => <li key={key}>{todo.text}
+            <input type='checkbox' checked={todo.check} onChange={e => this.todoChecked(todo, key)} ></input></li>)}
+        </ul>
+
+        <button onClick={e => this.removeTodo()}>Delete</button>
+      </div>
+
+    )
+  }
+
 }
-
-export default App;
